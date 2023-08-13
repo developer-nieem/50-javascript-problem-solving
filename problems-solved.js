d3f412048c8a9f04dc67ed2b894c5791eff23ed0
developer-nieem <naimhossenwork@gmail.com>
2023-08-13 21:56:38 +0600

    problem solved 2

--- problems-solved.js
+++ problems-solved.js
@@ -1,6 +1,6 @@
 // Find the largest number in an array.
 
-const findLargestNum = (arr) => {
+/* const findLargestNum = (arr) => {
         let max = arr[0];
     for (let i = 0; i < arr.length; i++) {
         
@@ -13,4 +13,14 @@ const findLargestNum = (arr) => {
     console.log(max);
 }
 
-findLargestNum([ 500,6,48,9656,56,])
+findLargestNum([ 500,6,48,9656,56,]) */
+
+
+//============== 2.  How to show 1 -6 numbers on ludu
+
+/* const luduKhala = (min, max) => {
+    const result =  Math.floor(Math.random() * (max - min + 1 ) + min );
+    return result
+}
+
+console.log(luduKhala(1 , 6));  */
